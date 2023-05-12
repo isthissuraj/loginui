3d87aee5918c868c588d76ac7adf19c569b03a04
Suraj <112235622+isthissuraj@users.noreply.github.com>
2023-05-12 22:53:18 +0530

    Update login_page.dart

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -175,10 +175,7 @@ class _LoginPageState extends State<LoginPage> {
  
   
 
-  //         }),
-  //   );
-  // }
-  //
+ 
   // loggedIn(LoginController model) {
   //   setState((){UserEmail = model.googleSignInAccount?.email;});
   //   Navigator.push(context,MaterialPageRoute(builder: (context)
