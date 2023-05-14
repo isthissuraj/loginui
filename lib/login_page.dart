1f030e8ec949d431cc6e2d9c4e4c2ceb4be3d68a
Suraj <112235622+isthissuraj@users.noreply.github.com>
2023-05-14 22:43:48 +0530

    Update login_page.dart

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -204,7 +204,6 @@ class _LoginPageState extends State<LoginPage> {
 
 
 
-//     await FirebaseAuth.instance.signInWithEmailAndPassword(
 //       email: _emailController.text.trim(),
 //       password: _passwordController.text.trim(),);
 //   }
